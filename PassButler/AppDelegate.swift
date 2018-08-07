aa4c75d9ea8dc31cff88d09ed7bf7b586de0dce3
Emanuele Guicciardi <eguicciardi@gmail.com>
2018-08-07 16:13:03 +0200

    Initial commit

---
+++ PassButler/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PassButler
+//
+//  Created by Emanuele Guicciardi on 07/08/2018.
+//  Copyright © 2018 Emanuele Guicciardi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
