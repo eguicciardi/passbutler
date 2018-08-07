738b3e405ac6ff35b37d613df044fd3a2dc43e7d
Emanuele Guicciardi <eguicciardi@gmail.com>
2018-08-07 22:00:00 +0200

    Version 1.0
    Moved to github

--- PassButler/ViewController.swift
+++ PassButler/ViewController.swift
@@ -9,7 +9,15 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    
+    let defaultPasswordNumber = 10;
+    let defaultPasswordLength = 6;
+    
+    @IBOutlet weak var textarea: NSTextField!   // Generated password outlet
+    @IBOutlet weak var passwordLengthInput: NSTextField!
+    @IBOutlet weak var passwordNumberInput: NSTextField!
+    @IBOutlet weak var specialCharsInput: NSButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +29,28 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
-
+    
+    @IBAction func generateButtonClicked(_ sender: Any) {
+        
+        let passwordNumber = passwordNumberInput.integerValue > 0 ? passwordNumberInput.integerValue : defaultPasswordNumber;
+        let passwordLength = passwordLengthInput.integerValue > 0 ? passwordLengthInput.integerValue : defaultPasswordLength;
+        
+        let passwordCharacters = Array("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890");
+        let specialCharacters = Array("?@!%=^*");
+        
+        var generatedPasswordArray = [String]();
+        
+        for _ in 1...passwordNumber {
+            
+            let randomPassword = String((0..<passwordLength).map{ _ in passwordCharacters[Int(arc4random_uniform(UInt32(passwordCharacters.count)))]})
+            
+            generatedPasswordArray.append(randomPassword);
+            
+        }
+        
+        textarea.stringValue = generatedPasswordArray.joined(separator: "\n");
+        
+    }
+    
 }
 
